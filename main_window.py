5fc5548d6626e6de82b959396df858971aa43a7b
vierzigundzwei@googlemail.com <vierzigundzwei@googlemail.com@bfc2f0ad-cc86-aca7-f4e1-f54902d43bf4>
2011-10-05 15:54:11 +0000

    * paralaxe

--- main_window.py
+++ main_window.py
@@ -16,52 +16,49 @@ NUMBER_OF_STARS = 1000
 class GameWindow(pyglet.window.Window):
     def __init__(self, **kwargs):
         super(GameWindow, self).__init__(**kwargs)
-        self.star_coords = []
-        #for i in xrange(NUMBER_OF_STARS):
-        #    self.star_coords.append(random.randint(0, self.width))
-        #    self.star_coords.append(random.randint(0, self.height))
-        self.randomize_stars()
-        self.stars = pyglet.graphics.vertex_list(len(self.star_coords) / 2,
-                                                 ('v2i', tuple(self.star_coords)))
         self.dragging = False
         self.camera_offset = [0, 0]
         self.zoom = 0
+        self.star_coords = []
+        self.randomize_stars()
+        self.update_stars()
 
     def on_mouse_drag(self, x, y, dx, dy, buttons, modifiers):
         if buttons & mouse.LEFT:
             self.dragging = True
             self.camera_offset[0] += dx
             self.camera_offset[1] += dy
+            self.update_stars()
 
     def on_mouse_release(self, x, y, buttons, modifiers):
         if buttons & mouse.LEFT and self.dragging:
             self.dragging = False
-            self.update_stars()
-                       
+
     def update_stars(self):
         x_offset = self.camera_offset[0] / 10
         y_offset = self.camera_offset[1] / 10
         verts = []
-        x = True
-        for vert in self.star_coords:
-            if x:
-                new = vert + x_offset
-                verts.append(new if new <= self.width else new - self.width)
+        for i, vert in enumerate(self.star_coords):
+            if i%2:
+                new = vert + y_offset * (max(1, ((i-1)/2)%5))
+                verts.append(new % self.height)
             else:
-                new = vert + y_offset
-                verts.append(new if new <= self.height else new - self.height)
-            x = not x
+                new = vert + x_offset * (max(1, (i/2)%5))
+                verts.append(new % self.width)
         self.stars.vertices = verts
         self.on_draw()
 
     def randomize_stars(self):
         self.star_coords = []
-        for i in xrange(NUMBER_OF_STARS):
-            self.star_coords.append(random.randint(0, self.height))
+        for i in xrange(self.width*self.height/1000):
             self.star_coords.append(random.randint(0, self.width))
-            
+            self.star_coords.append(random.randint(0, self.height))
+        self.stars = pyglet.graphics.vertex_list(len(self.star_coords) / 2,
+                                                 ('v2i', tuple(self.star_coords)))
+
     def on_resize(self, width, height):
         self.randomize_stars()
+        self.update_stars()
         super(GameWindow, self).on_resize(width, height)
 
     def on_key_press(self, symbol, modifiers):
@@ -70,6 +67,7 @@ class GameWindow(pyglet.window.Window):
     def on_draw(self):
         self.clear()
         self.stars.draw(pyglet.gl.GL_POINTS)
+
 
 if __name__ == '__main__':
     window = GameWindow(width=WINDOW_WIDTH, height=WINDOW_HEIGHT, resizable=True)
